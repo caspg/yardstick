f518bcba56a14b63a4cb23ad5b8f7c70c72c4448
piotr-s <piotr.slawinski1@gmail.com>
2018-09-19 22:49:15 +0200

    Remove commented code

--- src/lib/web/modules/home/breweries/breweries.controller.ts
+++ src/lib/web/modules/home/breweries/breweries.controller.ts
@@ -6,13 +6,6 @@ import { BreweryService } from '@app/core/brewery'
 class HomeBreweriesController {
   constructor(private readonly breweryService: BreweryService) {}
 
-  // @Get()
-  // @Render('home/breweries/index')
-  // async index() {
-  //   const breweries = await this.breweryService.findAll()
-  //   return { breweries }
-  // }
-
   @Get()
   async index(@Res() res) {
     res.render('home/breweries/index', {
