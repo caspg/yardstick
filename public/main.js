1e8074a31d63fc8f9fdf8b6eb1981a942f6437bd
piotr-s <piotr.slawinski1@gmail.com>
2018-09-23 18:30:02 +0200

    Add multiple statick markers to the map

---
+++ public/main.js
@@ -0,0 +1,22 @@
+function initMap() {
+  let locations = {
+    'Ye Olde Pub': { lat: 51, lng: 0 },
+    'Das Neue Brauhau': { lat: 52, lng: 13 },
+    'Stary Browar': { lat: 52, lng: 21 },
+    'Il Vecchio Birrificio': { lat: 41, lng: 12 },
+  }
+  var uluru = { lat: 51, lng: 0 }
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: locations['Ye Olde Pub'],
+  })
+
+  for (var key in locations) {
+    if (locations.hasOwnProperty(key)) {
+      var marker = new google.maps.Marker({
+        position: locations[key],
+        map: map,
+      })
+    }
+  }
+}
