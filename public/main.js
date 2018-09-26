6618605e3d061907189f07b9826616e0d2f3d4a4
piotr-s <piotr.slawinski1@gmail.com>
2018-09-26 18:54:46 +0200

    Change center location and add dummy search

--- public/main.js
+++ public/main.js
@@ -1,14 +1,14 @@
 function initMap() {
   let locations = {
     'Ye Olde Pub': { lat: 51, lng: 0 },
-    'Das Neue Brauhau': { lat: 52, lng: 13 },
+    'Das Neue Brauhaus': { lat: 52, lng: 13 },
     'Stary Browar': { lat: 52, lng: 21 },
     'Il Vecchio Birrificio': { lat: 41, lng: 12 },
   }
   var uluru = { lat: 51, lng: 0 }
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: locations['Ye Olde Pub'],
+    center: locations['Das Neue Brauhaus'],
   })
 
   for (var key in locations) {
